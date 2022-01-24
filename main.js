4ba30852807111ae0a2a6d8f9e22b8e14068348c
chris-fernandes <83762983+chris-fernandes@users.noreply.github.com>
2022-01-24 14:04:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+//main
+
+// apple - maraca
+// ball - ping pong ball
+// boat - apron
+// bottle - lipstick, lip rogue
+// bow - bow-tie, bowtie
+// cup - coffee mug
+// headphones - microphone, stethoscope
+// key - revolver, six gun, six shooter
+// pen - ball point, ballpoint pen, ball pen
+// scissor - vacuum, vacuum cleaner
+
+
+// apple - apple
+// ball - pink cricket leather ball
+// boat - cross stitch kit
+// bottle - pink cello sports bottle
+// bow - hair tie white and floral
+// cup - matt black coffee mug
+// headphones - boat wired headset
+// key - key ring steel
+// pen - classmate octane gel pens
+// scissor - steel scissor
+
